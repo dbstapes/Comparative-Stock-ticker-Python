090ab0ecdcf73c11c91dabfd424310ef3d0946df
dbstapes <devinstapleton3@gmail.com>
2022-02-12 15:35:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from datetime import date
 import yfinance as yf
 
 from ploty import graph_objs as go 
+
+import pandas as pd 
+import mplfinance as mpf 
+
+START = "2015-01-01"
+
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Comparative Stocks")
