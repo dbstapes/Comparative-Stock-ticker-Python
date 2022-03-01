fea0bd2e9c07cb21497b5f4e9c387d1cdf694227
dbstapes <devinstapleton3@gmail.com>
2022-03-01 16:19:03 -0500

    new gui ussage

--- main.py
+++ main.py
@@ -19,53 +19,52 @@ from plotly import graph_objs as go
 import pandas as pd 
 import mplfinance as mpf 
 
-i = 0
-stocks = ("AAPL", "NDAQ", "AMD", "TSLA", "FB", "INTC", "LOW", "HD")
+def graph(numerator, denominator, length, size):
+    i = 0
+    stocks = ("AAPL", "NDAQ", "AMD", "TSLA", "FB", "INTC", "LOW", "HD")
 
-#potencial stocks AAPL NDAQ AMD TSLA FB INTC LOW HD
-aple = yf.download( tickers = "AMD", period = "1y", interval = "1d" )
-nasdaq = yf.download( tickers = "NDAQ", period = "1y", interval = "1d" )
+    
+    aple = yf.download( tickers = numerator, period = length, interval = size )
+    nasdaq = yf.download( tickers = denominator, period = length, interval = size )
 
-aple.to_csv('AAPL.csv')
-nasdaq.to_csv('NDAQ.csv')
-aple.to_csv('AAPLtoNDAQ.csv')
+    aple.to_csv('AAPL.csv')
+    nasdaq.to_csv('NDAQ.csv')
+    aple.to_csv('AAPLtoNDAQ.csv')
 
-df = pd.read_csv('AAPLtoNDAQ.csv')
-cf = pd.read_csv('AAPL.csv')
-vf = pd.read_csv('NDAQ.csv')
+    df = pd.read_csv('AAPLtoNDAQ.csv')
+    cf = pd.read_csv('AAPL.csv')
+    vf = pd.read_csv('NDAQ.csv')
 
+    while(i < len(df)):
+        j = cf.loc[i, 'Open']
+        k = vf.loc[i, 'Open']
+        l = j/k
+        df.loc[i, 'Open'] = l
 
+        j = cf.loc[i, 'High']
+        k = vf.loc[i, 'High']
+        l = j/k
+        df.loc[i, 'High'] = l
 
-while(i < len(df)):
-    j = cf.loc[i, 'Open']
-    k = vf.loc[i, 'Open']
-    l = j/k
-    df.loc[i, 'Open'] = l
+        j = cf.loc[i, 'Low']
+        k = vf.loc[i, 'Low']
+        l = j/k
+        df.loc[i, 'Low'] = l
 
-    j = cf.loc[i, 'High']
-    k = vf.loc[i, 'High']
-    l = j/k
-    df.loc[i, 'High'] = l
+        j = cf.loc[i, 'Close']
+        k = vf.loc[i, 'Close']
+        l = j/k
+        df.loc[i, 'Close'] = l
 
-    j = cf.loc[i, 'Low']
-    k = vf.loc[i, 'Low']
-    l = j/k
-    df.loc[i, 'Low'] = l
-
-    j = cf.loc[i, 'Close']
-    k = vf.loc[i, 'Close']
-    l = j/k
-    df.loc[i, 'Close'] = l
-
-    j = cf.loc[i, 'Adj Close']
-    k = vf.loc[i, 'Adj Close']
-    l = j/k
-    df.loc[i, 'Adj Close'] = l
-    i += 1
-
+        j = cf.loc[i, 'Adj Close']
+        k = vf.loc[i, 'Adj Close']
+        l = j/k
+        df.loc[i, 'Adj Close'] = l
+        i += 1
 
 
-df.Date = pd.to_datetime(df.Date)
-df = df.set_index('Date')
-df.to_csv('AAPLtoNDAQ.csv')
-mpf.plot(df, type = 'candle', style = 'charles' )
+
+    df.Date = pd.to_datetime(df.Date)
+    df = df.set_index('Date')
+    df.to_csv('AAPLtoNDAQ.csv')
+    mpf.plot(df, type = 'candle', style = 'charles' )
--- window.py
+++ window.py
@@ -1,18 +1,29 @@
 from tkinter import *
+from main import *
 
+stock1 = ""
+stock2 = ""
+history = ""
+candle = ""
 
 def btn_clicked():
     print("Button Clicked")
+    stock1 = entry0.get()
+    stock2 = entry2.get()
+    history = entry1.get()
+    candle = entry3.get()
+    graph(stock1, stock2, history, candle)
+    print(entry0.get() + " " +  entry1.get() + " " + entry2.get() + " " + entry3.get())
 
 
 window = Tk()
 
-window.geometry("1200x900")
+window.geometry("1200x768")
 window.configure(bg = "#2d283e")
 canvas = Canvas(
     window,
     bg = "#2d283e",
-    height = 900,
+    height = 768,
     width = 1200,
     bd = 0,
     highlightthickness = 0,
@@ -21,37 +32,70 @@ canvas.place(x = 0, y = 0)
 
 entry0_img = PhotoImage(file = f"img_textBox0.png")
 entry0_bg = canvas.create_image(
-    259.0, 434.5,
+    210.5, 335.0,
     image = entry0_img)
 
 entry0 = Entry(
-    bd = 0,
+    bd = 3,
     bg = "#564f6f",
-    highlightthickness = 0)
+    highlightthickness = 0,
+
+    )
 
 entry0.place(
-    x = 82, y = 390,
-    width = 354,
-    height = 87)
+    x = 69, y = 303,
+    width = 283,
+    height = 62   
+    )
 
 entry1_img = PhotoImage(file = f"img_textBox1.png")
 entry1_bg = canvas.create_image(
-    940.0, 434.5,
+    210.5, 505.0,
     image = entry1_img)
 
 entry1 = Entry(
-    bd = 0,
+    bd = 3,
     bg = "#564f6f",
     highlightthickness = 0)
 
 entry1.place(
-    x = 763, y = 390,
-    width = 354,
-    height = 87)
+    x = 69, y = 473,
+    width = 283,
+    height = 62)
+
+entry2_img = PhotoImage(file = f"img_textBox2.png")
+entry2_bg = canvas.create_image(
+    931.5, 335.0,
+    image = entry2_img)
+
+entry2 = Entry(
+    bd = 3,
+    bg = "#564f6f",
+    highlightthickness = 0)
+
+entry2.place(
+    x = 790, y = 303,
+    width = 283,
+    height = 62)
+
+entry3_img = PhotoImage(file = f"img_textBox3.png")
+entry3_bg = canvas.create_image(
+    931.5, 505.0,
+    image = entry3_img)
+
+entry3 = Entry(
+    bd = 3,
+    bg = "#564f6f",
+    highlightthickness = 0)
+
+entry3.place(
+    x = 790, y = 473,
+    width = 283,
+    height = 62)
 
 background_img = PhotoImage(file = f"background.png")
 background = canvas.create_image(
-    521.0, 208.0,
+    496.5, 246.0,
     image=background_img)
 
 img0 = PhotoImage(file = f"img0.png")
@@ -63,9 +107,9 @@ b0 = Button(
     relief = "flat")
 
 b0.place(
-    x = 496, y = 687,
+    x = 483, y = 603,
     width = 208,
     height = 100)
 
-window.resizable(True, True)
+window.resizable(False, False)
 window.mainloop()
